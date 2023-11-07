fa8eeaa6717e75ebb6054a1972233e2a66786dc2
unknown <shahbaz_ali@outlook.in>
2023-11-08 02:53:35 +0530

    add route for fetching amenities

--- app/routes/copyAssist.routes.js
+++ app/routes/copyAssist.routes.js
@@ -17,7 +17,7 @@ const getPropertyData = async (address) => {
         maxBodyLength: Infinity,
         url: `https://api.rentcast.io/v1/properties?address=${address}`,
         headers: {
-            'x-api-key': '5b4a56d617574560920e07ac4da5e9b6'
+            'x-api-key': 'da65a87cbd2d4d208a90c9eb4d881358'
         }
     };
 
@@ -163,6 +163,56 @@ const getResponse = async (messages) => {
 }
 
 module.exports = function (app) {
+    app.post("/api/getRentCastData", async (req, res) => {
+        try {
+            const { address } = req.body;
+            console.log(address);
+            const propertyData = await getPropertyData(address);
+            res.send(propertyData)
+        } catch (error) {
+            console.log(error);
+            res.status(500).send("Something went Wrong");
+        }
+    })
+
+    /*
+     We'll need to reimplement the business logic later.
+    */
+    app.post("/api/getNearbyAmenities", async (req, res) => {
+        try {
+
+            const {
+                lat,
+                lng 
+            } = req.body;
+
+            console.log(lat, lng);
+
+            let config = {
+                method: "get",
+                maxBodyLength: Infinity,
+                url: `https://api.styldod.com/api_v2/ai-marketing-hub/get-near-by-amenities?lng=${lng}&lat=${lat}&radius=3000`,
+                headers: {
+                    Authorization:
+                        "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ0OWU0N2ZiZGQ0ZWUyNDE0Nzk2ZDhlMDhjZWY2YjU1ZDA3MDRlNGQiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiU2hhaGJheiBBbGkiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jTDBZTVh3aUg1ZEs3bEl0MFY3M1JxV0c5em5ycUhpY3lJXzA2bDk3RTkxX3c9czk2LWMiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vYjJjLXdlYi1mdWxsIiwiYXVkIjoiYjJjLXdlYi1mdWxsIiwiYXV0aF90aW1lIjoxNjk5Mzc5NTcxLCJ1c2VyX2lkIjoiZ3MzNnZyRHIyRVRvQXg3Mlk5eng0SnNsVmNxMSIsInN1YiI6ImdzMzZ2ckRyMkVUb0F4NzJZOXp4NEpzbFZjcTEiLCJpYXQiOjE2OTkzNzk1NzEsImV4cCI6MTY5OTM4MzE3MSwiZW1haWwiOiJzaGFoYmF6LmFsaS5jb25uZWN0QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7Imdvb2dsZS5jb20iOlsiMTEwNDQ1MjQyMzYyMzc4ODU1ODE4Il0sImVtYWlsIjpbInNoYWhiYXouYWxpLmNvbm5lY3RAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiZ29vZ2xlLmNvbSJ9fQ.LXov7EI_m0X9aKxUhi8L7l9flNEv4eagQUPzKQ3yZupz_-RBdFEwS3P0-TzbuVG_Eco_eKx0_eO0uXoFCMU2Idn03SuzOsFLlWWtvGq6xIqP9doY_yeDtUfpVK8s8Ly2rAIRCMwzpsoC5bBs2WipCY_7wCfzo7p6F_3sEDhsqmHB9bM4nFVHcBGgfBTGDmdgjnetrBpTNTqLyWT10vXRp1uKXOsN9fNizBC9gRUr_bXvu5o8JsypPEPW_Lf09ETonL4-M2U1pfwZCBxy-gTBjBS2OQDdjGWUji4_bfm40wd7oO0jEt0voW5CdhPHVWTs_bY68y_unSD1XVT54SjpOQ",
+                }, // This might get expired and need to be updated.
+            };
+
+            axios
+                .request(config)
+                .then((response) => {
+                    res.send(response.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).send("Something went Wrong");
+        }
+    })
+
     app.post("/api/copyAssist", async (req, res) => {
         try {
             const {
@@ -235,5 +285,5 @@ module.exports = function (app) {
             });
         }
     })
-    
+
 };
